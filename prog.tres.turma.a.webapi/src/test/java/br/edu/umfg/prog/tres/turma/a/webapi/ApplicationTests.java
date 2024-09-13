91e97007c4ae7cf4ae14156e12cbbd696d31464c
= <=>
2024-09-12 21:22:04 -0300

    1.0

---
+++ prog.tres.turma.a.webapi/src/test/java/br/edu/umfg/prog/tres/turma/a/webapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.umfg.prog.tres.turma.a.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
